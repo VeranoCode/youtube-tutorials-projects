0725943d89697d3e05bc8a69c87b32e13a8eb967
VeranoCode <158456063+VeranoCode@users.noreply.github.com>
2025-04-16 15:06:43 +0100

    Delete HTML-CSS-JS PROJECTS directory

--- HTML-CSS-JS PROJECTS/Auto Typing/Final/script.js
+++
@@ -1,9 +0,0 @@
-const typed = new Typed(".auto-type", {
-  strings: ["Developer", "Designer", "Freelance", "Content Creator"],
-  typeSpeed: 100,
-  backSpeed: 50,
-  loop: true,
-  backDelay: 1000,
-  cursorChar: "|",
-  smartBackspace: true,
-});
--- HTML-CSS-JS PROJECTS/Percentage Calculator/Starter/script.js
+++
@@ -1,5 +0,0 @@
-const calculateBtn = document.getElementById("calculateBtn");
-const numberInput = document.getElementById("number");
-const percentInput = document.getElementById("percent");
-const percentageResult = document.getElementById("percentageResult");
-const finalResult = document.getElementById("finalResult");
